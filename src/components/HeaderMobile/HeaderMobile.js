c5965318f0b1ae67f78a93aeed6fd96b702b7c82
dorofeeva-olga74 <dorofeeva.olga74@mail.ru>
2024-04-26 21:36:20 +0900

    responsive mobile header

--- src/components/HeaderMobile/HeaderMobile.js
+++ src/components/HeaderMobile/HeaderMobile.js
@@ -8,7 +8,7 @@ import Actions from '../../images/Vector.svg';
 import Logo from '../Logo/Logo.js';
 // import HeaderSectionsBottom from '../HeaderSectionsBottom/HeaderSectionsBottom.js';
 import Rectangle from '../../images/Rectangle.svg';
-// import HeaderSection from '../HeaderSection/HeaderSection.js';
+import HeaderSection from '../HeaderSection/HeaderSection.js';
 
 function HeaderMobile({ title, to }) {
   return (
@@ -18,7 +18,7 @@ function HeaderMobile({ title, to }) {
           <BurgerMenu />
           <SearchForm
             src={SearchMobile}
-            placeholder={'Поиск по сайту'}
+            placeholder={'Поиск'}
           />
           <button className='header-mobile__button'>
             <nav className='header-mobile__nav-box'>
@@ -46,18 +46,18 @@ function HeaderMobile({ title, to }) {
               src={Rectangle}
               alt='Угол вниз'
             />
+            <div className='header-mobile__sections'>
+              <nav className='sections__nav-box'>
+                <HeaderSection
+                  title='Латексные шары'
+                  to='/latex-balloons'
+                />
+              </nav>
+            </div>
             {/* <div className='header-mobile__sections'>
               <HeaderSectionsBottom />
             </div> */}
             <Catalog />
-            {/* <div className='header-mobile__sections'>
-                <nav className='sections__nav-box'>
-                  <HeaderSection
-                    title='Латексные шары'
-                    to='/latex-balloons'
-                  />
-                </nav>
-            </div> */}
           </div>
         </div>
       </header>
